f9bc96375ffbdea86494d9ddfd40a26fe3298de5
Gabriel Lulu <glulu4444@gmail.com>
2026-09-01 23:06:51 -0400

    added device sizes

--- next.config.mjs
+++ next.config.mjs
@@ -141,6 +141,7 @@ const nextConfig = {
     },
   },
   images: {
+    deviceSizes: [320, 480, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     remotePatterns: [
       {
         protocol: "https",
